c4436699e45dac3abdef72394a470f3b492a0025
Ramudgar <ramudgary52@gmail.com>
2023-09-29 10:40:34 +0545

    api for product add

--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -16,34 +16,40 @@ router.post(
   async (req, res) => {
     const data = req.body;
     const file = req.file;
+    const user = req.userData.role;
+    if (user === "admin") {
+      if (!data) {
+        return res
+          .status(400)
+          .json({ error: "Please enter all the required details" });
+      } else if (!file) {
+        return res.status(400).json({ error: "Please upload an image" });
+      } else {
+        try {
+          const image = domain + "/public/product/" + file.filename;
 
-    if (!data) {
-      return res
-        .status(400)
-        .json({ error: "Please enter all the required details" });
-    } else if (!file) {
-      return res.status(400).json({ error: "Please upload an image" });
-    } else {
-      try {
-        const image = domain + "/public/product/" + file.filename;
-
-        const product = new Product({
-          name: data.name,
-          category: data.category,
-          price: data.price,
-          quantity: data.quantity,
-          description: data.description,
-          image: image,
-        });
-        await product.save();
-        return res.status(200).json({
-          msg: "Product created successfully",
-          product,
-        });
-      } catch (err) {
-        console.error(err);
-        return res.status(500).json({ error: "Server Error" });
+          const product = new Product({
+            name: data.name,
+            category: data.category,
+            price: data.price,
+            quantity: data.quantity,
+            description: data.description,
+            image: image,
+          });
+          await product.save();
+          return res.status(200).json({
+            msg: "Product created successfully",
+            product,
+          });
+        } catch (err) {
+          console.error(err);
+          return res.status(500).json({ error: "Server Error" });
+        }
       }
+    } else {
+      return res
+        .status(401)
+        .json({ error: "You are not authorized to create a product" });
     }
   }
 );
