afb3402ee335d49b91f68ef371af68d9114a0d5d
Ramudgar <ramudgary52@gmail.com>
2023-10-02 10:37:18 +0545

    get and update routes are made for product api

--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -5,6 +5,7 @@ const Product = require("../models/productModel");
 const auth = require("../config/auth");
 const uploadServices = require("../services/uploadServices");
 const domain = "http://localhost:5000";
+const fs = require("fs");
 
 // @route POST product/create
 // @desc Create a product
@@ -15,13 +16,12 @@ router.post(
   uploadServices.productImage.single("image"),
   async (req, res) => {
     const data = req.body;
-    const category= await Category.find({name:data.category})
-    // store category_id to print from category 
-    const category_id=category[0]._id
+    const category = await Category.find({ name: data.category });
+    // store category_id to print from category
+    // console.log(category)
+    const category_id = category[0]._id;
     // console.log(category_id);
 
-
-    
     const file = req.file;
     const user = req.userData.role;
     if (user === "admin") {
@@ -57,6 +57,98 @@ router.post(
       return res
         .status(401)
         .json({ error: "You are not authorized to create a product" });
+    }
+  }
+);
+
+// @route GET products
+// @desc Get all products
+// @access Public
+router.get("/products/get", async (req, res) => {
+  try {
+    const products = await Product.find();
+    return res
+      .status(200)
+      .json({ msg: "products fetched successfully", products });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Server Error" });
+  }
+});
+
+// @route GET product/:id
+// @desc Get a product by ID
+// @access Public
+router.get("/product/get/:id", async (req, res) => {
+  const productId = req.params.id;
+
+  try {
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+    return res
+      .status(200)
+      .json({ msg: "product fetched successfully", product });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Server Error" });
+  }
+});
+
+// @route PUT product/update/:id
+// @desc Update a product by ID (including image)
+// @access Private (admin only)
+router.put(
+  "/product/update/:id",
+  auth.verifyUser,
+  uploadServices.productImage.single("image"), // Handle image upload
+  async (req, res) => {
+    const productId = req.params.id;
+    const data = req.body;
+
+    // Check if the user is an admin
+    if (req.userData.role !== "admin") {
+      return res
+        .status(401)
+        .json({ error: "You are not authorized to update a product" });
+    }
+
+    try {
+      const product = await Product.findById(productId);
+
+      if (!product) {
+        return res.status(404).json({ error: "Product not found" });
+      }
+
+      // Update the product properties with the provided data
+      if (data.name) product.name = data.name;
+      if (data.price) product.price = data.price;
+      if (data.quantity) product.quantity = data.quantity;
+      if (data.description) product.description = data.description;
+
+      // Update the image if a new image was uploaded
+      if (req.file) {
+        const oldImagePath = product.image;
+        const newImage = domain + "/public/product/" + req.file.filename;
+        product.image = newImage;
+
+        // Remove the old image file if it exists
+        if (fs.existsSync(oldImagePath)) {
+          fs.unlinkSync(oldImagePath);
+        }
+      }
+
+      // Save the updated product to the database
+      await product.save();
+
+      return res.status(200).json({
+        msg: "Product updated successfully",
+        product,
+      });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Server Error" });
     }
   }
 );
